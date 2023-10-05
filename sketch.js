ecd71e439a8b6a0e91aa5ff580ab2ae599c25676
raquidtrei1 <raquidtrei1@myvuw.ac.nz>
2023-10-05 19:49:31 +1300

    successfully created "quantization effect"

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ let sourceImg=null;
 let maskImg=null;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_2.png";
+let sourceFile = "input_5.jpg";
+let maskFile   = "mask_5.png";
+let outputFile = "output_5.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -22,37 +22,34 @@ function setup () {
   maskImg.loadPixels();
 }
 
-let X_STOP = 640;
-let Y_STOP = 480;
+let X_STOP = 1920;
+let Y_STOP = 1080;
 
 let renderCounter = 0;
 function draw () {
 
   let linesToDraw = 20;
-  let R = 0
-  let G = 0
-  let B = 0
-  let A = 255
 
   for(let j = renderCounter; j < renderCounter + linesToDraw && j < Y_STOP ; j++) {
     for(let i = 0; i < X_STOP; i++) {
       colorMode(RGB);
-      let pix = [R, G, B, A]
+      let pix = [0, 0, 0, 255]
       let mask = maskImg.get(i, j);
 
       if(mask[0] > 128){
         pix = sourceImg.get(i,j);
-        set(i,j,pix)
+        set(i,j,(Math.round(4*pix[0]/255)*64))
+        // print(Math.round(4*pix[2]/255)*64)
       }
       else {
         pix = sourceImg.get(i,j);
-        set(i,j,pix[0]-50)
+        set(i,j,pix)
       }
     }
   }
   renderCounter = renderCounter + linesToDraw;
   updatePixels();
-  print(renderCounter)
+
   if(renderCounter > Y_STOP) {
     console.log("Done!")
     noLoop();
